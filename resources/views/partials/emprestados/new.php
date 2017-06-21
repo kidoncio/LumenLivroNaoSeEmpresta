c2ba32da538dad6aec9b57ba940cdfbd7dde0bfa
Lucas Nascimento <lucas@lapou.com.br>
2017-06-21 01:02:34 -0300

    Permitir apenas livros que não foram emprestados

--- resources/views/partials/emprestados/new.php
+++ resources/views/partials/emprestados/new.php
@@ -16,7 +16,7 @@
                 <label class="col-md-4 control-label" for="livro_id">Livro</label>
                 <div class="col-md-4">
                     <select id="livro_id" name="livro_id" class="form-control" ng-model="emprestado.livro_id">
-                        <option ng-repeat="livro in livros" value="{{ livro.id }}">{{ livro.titulo }}</option>
+                            <option ng-repeat="livro in livros" ng-if="livro.disponivel == '1' && livro.emprestado.nome == null" value="{{ livro.id }}">{{ livro.titulo }}</option>
                     </select>
                 </div>
             </div>
