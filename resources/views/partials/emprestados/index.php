57e9e7e93a8bc1537a70a5ae8b6fbd9cc24bbf83
Lucas Nascimento <lucas@lapou.com.br>
2017-06-21 01:02:15 -0300

    :art: Melhorar exibição dos livros emprestados

--- resources/views/partials/emprestados/index.php
+++ resources/views/partials/emprestados/index.php
@@ -1,24 +1,35 @@
 <div ng-controller="emprestadoCtrl" data-ng-init="getEmprestados()">
 
-    <table class="table">
+    <form>
+        <div class="form-group">
+            <div class="input-group">
+                <div class="input-group-addon"><i class="fa fa-search"></i></div>
+                <input type="text" class="form-control" placeholder="Pesquisar Livro" ng-model="searchTitulo">
+            </div>
+        </div>
+    </form>
+
+    <table class="table table-bordered table-striped">
         <thead>
         <tr>
-            <th>Livro</th>
-            <th>Emprestado para</th>
+            <th>
+                Livro
+            </th>
+            <th>
+                Emprestado para
+            </th>
             <th>Ações</th>
         </tr>
         </thead>
 
         <tbody ng-scope>
-        <tr data-ng-repeat="emprestado in emprestados">
-            <div ng-if="emprestado.livro.disponivel == '1'">
-                <td>{{ emprestado.livro.titulo }}</td>
-                <td>{{ emprestado.nome }}</td>
-                <td>
-                    <a class="btn btn-success" ng-href='{{"/emprestado/"+emprestado.id}}'><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
-                    <button class="btn btn-danger" ng-click="emprestadoDelete(emprestado.id)"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-                </td>
-            </div>
+        <tr data-ng-repeat="emprestado in emprestados | filter:searchTitulo">
+            <td>{{ emprestado.livro.titulo }}</td>
+            <td>{{ emprestado.nome }}</td>
+            <td>
+                <a class="btn btn-success" ng-href='{{"/emprestado/"+emprestado.id}}'><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></a>
+                <button class="btn btn-danger" ng-click="emprestadoDelete(emprestado.id)"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+            </td>
         </tr>
         </tbody>
     </table>
