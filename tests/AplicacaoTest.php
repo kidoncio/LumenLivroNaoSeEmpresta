02e72a9df4ded51f780fa5f25a979865c0b7bfc2
Lucas Nascimento <lucas@lapou.com.br>
2017-06-21 14:12:00 -0300

    Update Teste da Aplicação

--- tests/AplicacaoTest.php
+++ tests/AplicacaoTest.php
@@ -3,7 +3,7 @@
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class AplicacaoTest extends TestCase
 {
 
     /** @test */
